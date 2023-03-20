9ab46bc913672914428d7f20c56e7e44228f5a63
Rafael Fontana Becker Gois <joefontanabg@gmail.com>
2023-03-19 21:26:27 -0300

    update signos

---
+++ src/components/itemLista/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ItemLista = (props) => {
+    return (
+        <div className="boxSigno">
+            <div className="nomeSigno">
+                {props.signo}
+            </div>
+
+            <img className="imgSigno" src={props.imagem} alt={props.signo} />
+
+            <div className="periodoNascimento">
+                {props.dataInicio} - {props.dataFim}
+            </div>
+        </div>
+    )
+}
+
+export default ItemLista;
