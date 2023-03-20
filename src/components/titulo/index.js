096799fd03c5cab466c92f6dd73072f2238880d2
MendesMaquiansPCP <rafael.gois@mendesmaquinas.com.br>
2023-03-20 11:03:41 -0300

    update layout

---
+++ src/components/titulo/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './styles.css'
+
+const Titulo = () => {
+    return (
+        <div className="box">
+            <p className="texto">Baralho de Signos</p>
+            
+        </div>
+    )
+}
+
+export default Titulo;
